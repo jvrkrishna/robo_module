606f79a08c3a5241fa770fd7754ba6066bba2efb
Rama Krishna <jvrkrishnaa@gmail.com>
2023-12-15 17:59:19 +0530

    a

--- main.tf
+++ main.tf
@@ -1,5 +1,38 @@
-resource "null_resource" "test" {
-  provisioner "local-exec" {
-    command = "echo Hello World - Env is ${var.env}"
-  }
+## I am policy
+resource "aws_iam_policy" "policy" {
+  name        = "${var.component}-${var.env}-ssm-policy"
+  path        = "/"
+  description = "${var.component}-${var.env}-ssm-policy"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ssm:GetParameter*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+## I am Role
+resource "aws_iam_role" "role" {
+  name = "${var.component}-${var.env}-Ec2-Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
 }
