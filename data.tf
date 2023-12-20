92c5b66727478f08be5492083615ac43b53cb207
Rama Krishna <jvrkrishnaa@gmail.com>
2023-12-20 11:08:25 +0530

    a

--- data.tf
+++ data.tf
@@ -1,5 +1,5 @@
 ############ aws ami id datasource terraform #############
-data "aws_ami" "example" {
+data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
--- main.tf
+++ main.tf
@@ -1,97 +1,75 @@
-############### I am Policy ####################
+########### Create I am policy in terraform ############
 resource "aws_iam_policy" "policy" {
-  name        = "${var.component}.${var.env}.ssm.policy"
+  name        = "${var.component}-${var.env}-ssm-pm-policy"
   path        = "/"
-  description = "My test policy"
+  description = "${var.component}-${var.env}-ssm-pm-policy"
 
-  policy = jsonencode({
+  policy = <<EOF
+{
     "Version": "2012-10-17",
     "Statement": [
-      {
-        "Sid": "VisualEditor0",
-        "Effect": "Allow",
-        "Action": [
-          "ssm:GetParameterHistory",
-          "ssm:GetParametersByPath",
-          "ssm:GetParameters",
-          "ssm:GetParameter",
-          "ssm:DescribeParameters"
-        ],
-        "Resource": "arn:aws:ssm:us-east-1:207072006229:parameter/roboshop.${var.env}.${var.component}.*"
-      }
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "ssm:GetParameterHistory",
+                "ssm:GetParametersByPath",
+                "ssm:GetParameters",
+                "ssm:GetParameter",
+                "ssm:DescribeParameters"
+            ],
+            "Resource": "arn:aws:ssm:us-east-1:207072006229:parameter/roboshop.${var.env}.${var.component}.*"
+        }
     ]
-  })
+}
+EOF
 }
 
-################ I am role ##################
+############# Create I am role in terraform ######################
 resource "aws_iam_role" "role" {
-  name = "${var.component}.${var.env}.ec2role"
-
-  assume_role_policy = jsonencode({
+  name = "${var.component}-${var.env}-ec2-role"
+  assume_role_policy = <<EOF
+{
     "Version": "2012-10-17",
     "Statement": [
-      {
-        "Effect": "Allow",
-        "Principal": {
-          "Service": "ec2.amazonaws.com"
-        },
-        "Action": "sts:AssumeRole"
-      }
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
     ]
-  })
 }
-################ I am instance profile#####################
-resource "aws_iam_instance_profile" "profile" {
-  name = "${var.component}.${var.env}"
+EOF
+}
+############### Create I am instance profile ###################
+resource "aws_iam_instance_profile" "instance_profile" {
+  name = "${var.component}-${var.env}-instance-profile"
   role = aws_iam_role.role.name
 }
-################ Policy attachment##############
-resource "aws_iam_role_policy_attachment" "attach" {
+############### Create Policy attachment in terraform ##############
+resource "aws_iam_role_policy_attachment" "policy-attach" {
   role       = aws_iam_role.role.name
   policy_arn = aws_iam_policy.policy.arn
 }
 
-###### create ec2 instance terraform with vpc######
-resource "aws_instance" "web" {
-  ami                    = data.aws_ami.example.id
-  instance_type          = "t2.micro"
+########### Create ec2 instance in terraform #############
+resource "aws_instance" "instance" {
+  ami           = data.aws_ami.ami.id
+  instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.sg.id]
-  iam_instance_profile = aws_iam_instance_profile.profile.name
+  iam_instance_profile = aws_iam_instance_profile.instance_profile.name
 
   tags = {
-    Name = "${var.component}.${var.env}"
+    Name = "${var.component}-${var.env}"
   }
 }
 
-################ creating provisioner with null resource ################
-resource "null_resource" "ansible" {
-  depends_on = [aws_instance.web, aws_route53_record.www]   #### depends on this will create after this tasks
-  provisioner "remote-exec" {
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = aws_instance.web.public_ip
-    }
-    inline = [
-      "sudo labauto ansible",
-      "ansible-pull -i localhost, -U https://github.com/jvrkrishna/robo-ansible roboshop.yml -e env=dev -e role_name=${var.component}"
-    ]
-  }
-}
-################# creating dns records #################
-resource "aws_route53_record" "www" {
-  zone_id = "Z0858447245XTBTK7DY06"
-  name    = "${var.component}.${var.env}"
-  type    = "A"
-  ttl     = 30
-  records = [aws_instance.web.private_ip]
-}
-
-######### Security group terraform ##########
+############# Create vps security group in terraform ##############
 resource "aws_security_group" "sg" {
-  name        = "${var.component}.${var.env}"
-  description = "Allow TLS inbound traffic"
+  name        = "${var.component}-${var.env}-sg"
+  description = "${var.component}-${var.env}-sg"
 
   ingress {
     from_port        = 0
@@ -108,10 +86,32 @@ resource "aws_security_group" "sg" {
   }
 
   tags = {
-    Name = "${var.component}.${var.env}"
+    Name = "${var.component}-${var.env}-sg"
   }
 }
 
+############ create DNS Records for instances in teraform ###########
+resource "aws_route53_record" "www" {
+  zone_id = "Z0858447245XTBTK7DY06"
+  name    = "${var.component}-${var.env}"
+  type    = "A"
+  ttl     = 30
+  records = [aws_instance.instance.private_ip]
+}
 
-
-
+############## Provisioner for remote in terraform ################
+resource "null_resource" "ansible" {
+  depends_on = [aws_instance.instance, aws_route53_record.www]
+  provisioner "remote-exec" {
+    connection {
+      type     = "ssh"
+      user     = "centos"
+      password = "DevOps321"
+      host     = aws_instance.instance.public_ip
+    }
+    inline = [
+      "sudo labauto ansible",
+      "ansible-pull -i localhost, -U https://github.com/jvrkrishna/robo-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component}"
+    ]
+  }
+}
