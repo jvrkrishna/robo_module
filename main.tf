32b29f315367c173ef2411ffe024dc87a8f6c2a1
Rama Krishna <jvrkrishnaa@gmail.com>
2023-12-21 12:45:04 +0530

    a

--- main.tf
+++ main.tf
@@ -111,7 +111,7 @@ resource "null_resource" "ansible" {
     }
     inline = [
       "sudo labauto ansible",
-      "set-hostname ${var.component}",
+      "set-hostname -skip-apply ${var.component}",
       "ansible-pull -i localhost, -U https://github.com/jvrkrishna/robo-ansible roboshop.yml -e env=${var.env} -e role_name=${var.component}"
     ]
   }
